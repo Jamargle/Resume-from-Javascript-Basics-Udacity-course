c8ef7754c35f54e493a0b2a16f8c7d184a03af7d
José López <jmlb0003@red.ujaen.es>
2015-08-29 12:34:23 +0200

    First object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,3 +11,20 @@ var formattedName = HTMLheadername.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var bio = {
+	"name" : "Jose López",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "650-55-55-55",
+		"email" : "jmlb0003@jemail.com",
+		"github" : "jamargle",
+		"twitter" : "jmlb0003",
+		"location" : "Cambil"
+	},
+	"welcomeMessage" : "lorem ipsum dolor sit....",
+	"skills" : [
+		"awesomeness", "delivering things", "etc"
+	],
+	"bioPic" : "images/fry.jpg"
+};

5918d1de73ca7f6a9652dd7e214a39d72770a455
José López <jmlb0003@red.ujaen.es>
2015-08-29 11:48:26 +0200

    Replacing some %data% for adding name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,3 +5,9 @@ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
 console.log(awesomeThoughts);
 console.log(funThoughts);
+/**********************************/
+var name
+var formattedName = HTMLheadername.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);

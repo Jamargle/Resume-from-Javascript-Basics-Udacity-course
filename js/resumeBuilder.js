b1599c52847bfe20aba5c5420043341b1203bfb3
José López <jmlb0003@red.ujaen.es>
2015-08-29 02:18:28 +0200

    Replace function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,7 @@
 $("#main").append("Jose López");
 
-var awesomeThoughts = "I am Jose" + " and I am AWESOME!";
+var awesomeThoughts = "I am Jose and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 
 console.log(awesomeThoughts);
+console.log(funThoughts);
